1d1e4d00d354371ad55a2e252e7a55dd9b58b68c
InTheAxis <codeaxex@gmail.com>
2018-08-23 02:54:08 +0800

    made it chec konly 4 directions

--- SP1Framework/aStar.cpp
+++ SP1Framework/aStar.cpp
@@ -28,12 +28,12 @@ vector<Node> aStar::makePath(vector<Node> &map, Node dest)
 	vector<Node> path;
 	
 
-	while (!(map[c.Y * mapSize.X + c.X].parent.X == c.X && map[c.Y * mapSize.X + c.X].parent.Y == c.Y)
-		&& map[c.Y * mapSize.X + c.X].X != -1 && map[c.Y * mapSize.X + c.X].Y != -1)
+	while (!(map[(c.Y - 1 ) * mapSize.X + c.X].parent.X == c.X && map[(c.Y - 1 ) * mapSize.X + c.X].parent.Y == c.Y)
+		&& map[(c.Y - 1 ) * mapSize.X + c.X].X != -1 && map[(c.Y - 1 ) * mapSize.X + c.X].Y != -1)
 	{
-		path.push_back(map[c.Y * mapSize.X + c.X]);
-		int tempX = map[c.Y * mapSize.X + c.X].parent.X;
-		int tempY = map[c.Y * mapSize.X + c.X].parent.Y;
+		path.push_back(map[(c.Y - 1 ) * mapSize.X + c.X]);
+		int tempX = map[(c.Y - 1 ) * mapSize.X + c.X].parent.X;
+		int tempY = map[(c.Y - 1 ) * mapSize.X + c.X].parent.Y;
 		c.X = tempX;
 		c.Y = tempY;
 	}
@@ -48,24 +48,24 @@ vector<Node> aStar::aStarSearch(Node player, Node dest)
 {
 	//catch if nodes are within console or dest is reached
 	vector<Node> empty;
-	if (!isValid({dest.X, dest.Y}))
+	if (!isValid(dest) || !isValid(player))
 		return empty;
-	if (isDest({ player.X, player.Y }, dest))
+	if (isDest(player , dest))
 		return empty;
 
 	//init closedlist and map
-	vector<bool> closedList = vector<bool>(mapSize.X * mapSize.Y + 1);
-	vector<Node> map = vector<Node> (mapSize.X * mapSize.Y + 1);
+	vector<bool> closedList = vector<bool>(mapSize.X * mapSize.Y);
+	vector<Node> map = vector<Node> (mapSize.X * mapSize.Y );
 	for (int x = 0; x < mapSize.X; x++)
 	{
-		for (int y = 0; y < mapSize.Y; y++)
+		for (int y = 1; y < mapSize.Y; y++)
 		{
-			map[y * mapSize.X + x].fCost = FLT_MAX;
-			map[y * mapSize.X + x].gCost = FLT_MAX;
-			map[y * mapSize.X + x].hCost = FLT_MAX;
-			map[y * mapSize.X + x].parent = { -1, -1 };
-			map[y * mapSize.X + x].X = (SHORT)x;
-			map[y * mapSize.X + x].Y = (SHORT)y;
+			map[(y - 1) * mapSize.X + x].fCost = FLT_MAX;
+			map[(y - 1) * mapSize.X + x].gCost = FLT_MAX;
+			map[(y - 1) * mapSize.X + x].hCost = FLT_MAX;
+			map[(y - 1) * mapSize.X + x].parent = { -1, -1 };
+			map[(y - 1) * mapSize.X + x].X = (SHORT)x;
+			map[(y - 1) * mapSize.X + x].Y = (SHORT)y;
 
 			closedList[y * mapSize.X + x] = false;
 		}
@@ -75,16 +75,16 @@ vector<Node> aStar::aStarSearch(Node player, Node dest)
 
 	//init player and add it, the root node, to open list
 	Node c = player;
-	map[c.Y * mapSize.X + c.X].fCost = 0.0;
-	map[c.Y * mapSize.X + c.X].gCost = 0.0;
-	map[c.Y * mapSize.X + c.X].hCost = 0.0;
-	map[c.Y * mapSize.X + c.X].parent = c;
+	map[(c.Y - 1) * mapSize.X + c.X].fCost = 0.0;
+	map[(c.Y - 1) * mapSize.X + c.X].gCost = 0.0;
+	map[(c.Y - 1) * mapSize.X + c.X].hCost = 0.0;
+	map[(c.Y - 1) * mapSize.X + c.X].parent = c;
 
-	openList.emplace_back(map[c.Y * mapSize.X + c.X]);
+	openList.emplace_back(map[(c.Y - 1) * mapSize.X + c.X]);
 	bool destFound = false;
 
 	//main alg loop
-	while (!openList.empty() && openList.size() < (unsigned int)(mapSize.X * mapSize.Y + 1))
+	while (!openList.empty() && openList.size() < (unsigned int)(mapSize.X * mapSize.Y))
 	{
 		//find lowest fcost node and store it in node
 		Node node;
@@ -102,44 +102,62 @@ vector<Node> aStar::aStarSearch(Node player, Node dest)
 		//switch current node to node with lowest fcost
 		c = node;
 		//add to closedlist
-		closedList[c.Y * mapSize.X + c.X] = true;
+		closedList[(c.Y - 1)* mapSize.X + c.X] = true;
 
-		//iterate through the 8 nodes around this node
-		for (int newX = -1; newX <= 1; newX++) 
+		//iterate through the 4 nodes around this node
+		int newX = -1;
+		int newY = 0;
+		for (int i = 0; i < 4; i++) 
 		{
-			for (int newY = -1; newY <= 1; newY++) 
+			switch (i)
 			{
-				double gNew, hNew, fNew;
-				if (isValid({ (SHORT)(c.X + newX), (SHORT)(c.Y + newY) }))
+			case 0:
+				newX = -1;
+				newY = 0;
+				break;
+			case 1:
+				newX = 0;
+				newY = -1;
+				break;
+			case 2:
+				newX = 0;
+				newY = 1;
+				break;
+			case 3:
+				newX = 1;
+				newY = 0;
+				break;
+			}
+			double gNew, hNew, fNew;
+			if (isValid({ (SHORT)(c.X + newX), (SHORT)(c.Y + newY) }))
+			{
+				if (isDest({ (SHORT)(c.X + newX), (SHORT)(c.Y + newY) }, dest))
 				{
-					if (isDest({ (SHORT)(c.X + newX), (SHORT)(c.Y + newY) }, dest))
+					//set current node to be its own parent
+					map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].parent.X = c.X;
+					map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].parent.Y = c.Y;
+					destFound = true;
+					return makePath(map, dest);
+				}
+				else if (!closedList[(c.Y + newY - 1) * mapSize.X + (c.X + newX)]) //if not in closed list
+				{
+					//calculate fcost
+					gNew = node.gCost + 1.0;
+					hNew = calcH({ (SHORT)(c.X + newX), (SHORT)(c.Y + newY) }, dest);
+					fNew = gNew + hNew;
+					//check if this neighbour is in openlist or if uninitialized ie check all neighbours not checked
+					if (map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].fCost == FLT_MAX ||
+						map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].fCost > fNew)
 					{
-						//set current node to be its own parent
-						map[(c.Y + newY) * mapSize.X + (c.X + newX)].parent.X = c.X;
-						map[(c.Y + newY) * mapSize.X + (c.X + newX)].parent.Y = c.Y;
-						destFound = true;
-						return makePath(map, dest);
-					}
-					else if (!closedList[(c.Y + newY) * mapSize.X + (c.X + newX)]) //if not in closed list
-					{
-						//calculate fcost
-						gNew = node.gCost + 1.0;
-						hNew = calcH({ (SHORT)(c.X + newX), (SHORT)(c.Y + newY) }, dest);
-						fNew = gNew + hNew;
-						//check if this neighbour is in openlist or if uninitialized ie check all neighbours not checked
-						if (map[(c.Y + newY) * mapSize.X + (c.X + newX)].fCost == FLT_MAX ||
-							map[(c.Y + newY) * mapSize.X + (c.X + newX)].fCost > fNew)
-						{
-							//updates the cost of this neighbour
-							map[(c.Y + newY) * mapSize.X + (c.X + newX)].fCost = fNew;
-							map[(c.Y + newY) * mapSize.X + (c.X + newX)].gCost = gNew;
-							map[(c.Y + newY) * mapSize.X + (c.X + newX)].hCost = hNew;
-							//make current node the parent of this neighbour node
-							map[(c.Y + newY) * mapSize.X + (c.X + newX)].parent.X = c.X;
-							map[(c.Y + newY) * mapSize.X + (c.X + newX)].parent.Y = c.Y;
-							//add to openlist
-							openList.emplace_back(map[(c.Y + newY) * mapSize.X + (c.X + newX)]);
-						}
+						//updates the cost of this neighbour
+						map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].fCost = fNew;
+						map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].gCost = gNew;
+						map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].hCost = hNew;
+						//make current node the parent of this neighbour node
+						map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].parent.X = c.X;
+						map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)].parent.Y = c.Y;
+						//add to openlist
+						openList.emplace_back(map[(c.Y + newY - 1) * mapSize.X + (c.X + newX)]);
 					}
 				}
 			}
